0708b3a1adae5fdca20049af6a21748dd7bf9604
Nich <neikoo_n@yahoo.com>
2022-05-26 15:03:22 +0430

    in progress

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let intervalnum = document.querySelector(".interval");
-console.log(intervalnum);
+let counter = intervalnum.textContent;
+let counter2 = counter.replace(/[","]/g , "");
+console.log(counter2);
+let transaction = Number(counter2) + 1;
+console.log(transaction);
+console.log(typeof counter);
+
+
+
+// setInterval(()=> {
+//     innerhtml(transaction+=3)
+// }, 2000);
