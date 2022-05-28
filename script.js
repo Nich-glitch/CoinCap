e203d4ff2dfddaa310409a4a1e39351c13ad2f39
Nich <neikoo_n@yahoo.com>
2022-05-28 19:56:58 +0430

    apps background

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let counter = intervalnum.textContent;
 let counter2 = counter.replace(/[","]/g , "");
 console.log(counter2);
 let transaction = Number(counter2) + 1;
-console.log(transaction);
-console.log(typeof counter);
+// console.log(transaction);
+// console.log(typeof counter);
 
 
-
-// setInterval(()=> {
-//     innerhtml(transaction+=3)
-// }, 2000);
+setInterval(()=> {
+    // console.log(transaction+=3);
+    intervalnum.textContent=transaction+=3;
+    
+}, 2000);
